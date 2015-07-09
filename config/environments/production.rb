e4dcc074c5f53f73de8f21c092d9ee7379eebcf0
Akash Trivedi <dev202@outlook.com>
2015-07-09 05:30:04 -0700

    added pin views

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+    # Required for heroku
+    #note to set this to actual host
+  config.action_mailer.default_url_options = { :host => 'pintastic.herokuapp.com' }
+
 end
